43c55be7d8b530fd082138806b5213f615ef5f87
smschijf <210578@student.glu.nl>
2023-02-09 16:12:29 +0100

    initial commit

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello first request");
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend");
+});
