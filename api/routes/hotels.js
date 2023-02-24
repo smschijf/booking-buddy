44d15900b66c63e27251060fd727c81752a25f5f
smschijf <210578@student.glu.nl>
2023-02-24 14:55:31 +0100

    node.js and mongoDB authentication(login/register) done

---
+++ api/routes/hotels.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+
+const router = express.Router();
+
+// Create
+router.post("/", createHotel);
+
+// Update
+router.put("/:id", updateHotel);
+
+// Delete
+router.delete("/:id", deleteHotel);
+
+// Get
+router.get("/:id", getHotel);
+
+// Get all
+router.get("/", getHotels);
+
+export default router;
