b14fba36702871474c346e67350c461f2493ddab
smschijf <210578@student.glu.nl>
2023-02-28 14:22:08 +0100

    role based authorization and verification added with jsonwebtokens

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,18 +1,36 @@
 import express from "express";
-import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js"
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("Hello user, you are logged in");
+// });
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("Hello user, you are logged in and can delete your acount");
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("Hello admin, you are logged in and can delete all acounts");
+// });
+
 // Update
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 // Delete
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 // Get
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 // Get all
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
