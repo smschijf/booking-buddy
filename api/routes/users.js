e813ee906cecba651bf59e9b1debcdeea05e0524
smschijf <210578@student.glu.nl>
2023-02-28 13:50:58 +0100

    jsonwebtokens implementation with cookies

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,18 @@
 import express from "express";
+import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js"
 
 const router = express.Router();
 
+// Update
+router.put("/:id", updateUser);
+
+// Delete
+router.delete("/:id", deleteUser);
+
+// Get
+router.get("/:id", getUser);
+
+// Get all
+router.get("/", getUsers);
 
 export default router;
