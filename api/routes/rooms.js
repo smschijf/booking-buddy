936c6141e06f197fefdcdd1221635963ba92f262
smschijf <210578@student.glu.nl>
2023-02-28 15:32:33 +0100

    api calls upgraded with CRUD functionality for rooms

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,22 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// Create
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+// Update
+router.put("/:id", verifyAdmin, updateRoom);
+
+// Delete
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+// Get
+router.get("/:id", getRoom);
+
+// Get all
+router.get("/", getRooms);
 
 export default router;
