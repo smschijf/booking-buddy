738bd93d87f161e5e5ce5ca5173d245c9036157d
smschijf <210578@student.glu.nl>
2023-03-03 13:26:39 +0100

    fetch hotels countByCity from api with custom hook and axios

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -25,6 +26,7 @@ mongoose.connection.on("disconnected", () => {
 
 // middlewares
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
