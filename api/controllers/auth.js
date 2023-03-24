b51add9154cd300f63453867b9ab8b2ec845c686
smschijf <210578@student.glu.nl>
2023-03-24 13:41:39 +0100

    login authentication with context API

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -9,29 +9,27 @@ export const register = async (req, res, next) => {
     const hash = bcrypt.hashSync(req.body.password, salt);
 
     const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
+      ...req.body,
       password: hash,
     });
 
     await newUser.save();
-    res.status(200).send("User has been created");
+    res.status(200).send("User has been created.");
   } catch (err) {
     next(err);
   }
 };
-
 export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if (!user) return next(createError(404, "User not found"));
+    if (!user) return next(createError(404, "User not found!"));
 
     const isPasswordCorrect = await bcrypt.compare(
       req.body.password,
       user.password
     );
     if (!isPasswordCorrect)
-      return next(createError(400, "Wrong password or username"));
+      return next(createError(400, "Wrong password or username!"));
 
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
@@ -40,11 +38,11 @@ export const login = async (req, res, next) => {
 
     const { password, isAdmin, ...otherDetails } = user._doc;
     res
-      .cookie("acces_token", token, {
+      .cookie("access_token", token, {
         httpOnly: true,
       })
       .status(200)
-      .json({ ...otherDetails });
+      .json({ details: { ...otherDetails }, isAdmin });
   } catch (err) {
     next(err);
   }
